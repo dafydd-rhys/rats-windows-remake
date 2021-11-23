434cad8678c12e65929c620216a090201981cf0d
harryboyce2011556 <2011556@swansea.ac.uk>
2021-11-23 17:21:56 +0000

    Updated Female and Male Sex Change
    - Now searches through the entities they shares a tile with and applies the sex change to the first rat in the list of entities on the tile
    - Now removed from the list of entities on the tile when they have been used

--- src/entity/weapon/FemaleSexChange.java
+++ src/entity/weapon/FemaleSexChange.java
@@ -1,6 +1,11 @@
 package entity.weapon;
 
 import entity.rats.Rat;
+import entity.Entity;
+import main.level.Level;
+import tile.Tile;
+
+import java.util.ArrayList;
 
 /**
  * FemaleSexChange
@@ -20,12 +25,26 @@ public class FemaleSexChange extends Item {
         this.isAttackable = false;
     }
 
-    public void activate(Rat targetRat) {
-        if (!targetRat.isFemale()) {
-            targetRat.setFemale(true);
-            //add sprite change
+    public void activate() {
+
+        Tile[][] tile = Level.getTiles();
+        ArrayList<Entity> entitiesOnTile = tile[this.currentPosY][this.currentPosX].getEntitiesOnTile();
+
+        if (entitiesOnTile != null) {
+            for (int k = 0; k < entitiesOnTile.size(); k++) {
+                if (entitiesOnTile.get(k).getEntityName().equals("Rat")){
+                    Rat targetRat = (Rat) entitiesOnTile.get(k);
+                    if (!targetRat.isFemale()) {
+                        targetRat.setFemale(true);
+                        //add sprite change
+                    }
+                    this.hp -= 1;
+                    tile[this.currentPosY][this.currentPosX].removeEntityFromTile(this);
+                    break;
+                }
+            }
         }
-        this.hp -= 1;
+
     }
 
 }
--- src/entity/weapon/MaleSexChange.java
+++ src/entity/weapon/MaleSexChange.java
@@ -1,6 +1,11 @@
 package entity.weapon;
 
 import entity.rats.Rat;
+import entity.Entity;
+import main.level.Level;
+import tile.Tile;
+
+import java.util.ArrayList;
 
 /**
  * MaleSexChange
@@ -20,13 +25,26 @@ public class MaleSexChange extends Item {
         this.isAttackable = false;
     }
 
-    public void activate(Rat targetRat) {
-        if (targetRat.isFemale()) {
-            targetRat.setFemale(false);
-            //add sprite change
+    public void activate() {
+
+        Tile[][] tile = Level.getTiles();
+        ArrayList<Entity> entitiesOnTile = tile[this.currentPosY][this.currentPosX].getEntitiesOnTile();
+
+        if (entitiesOnTile != null) {
+            for (int k = 0; k < entitiesOnTile.size(); k++) {
+                if (entitiesOnTile.get(k).getEntityName().equals("Rat")){
+                    Rat targetRat = (Rat) entitiesOnTile.get(k);
+                    if (targetRat.isFemale()) {
+                        targetRat.setFemale(false);
+                        //add sprite change
+                    }
+                    this.hp -= 1;
+                    tile[this.currentPosY][this.currentPosX].removeEntityFromTile(this);
+                    break;
+                }
+            }
         }
-        this.hp -= 1;
-        // remove
+
     }
 
 }
