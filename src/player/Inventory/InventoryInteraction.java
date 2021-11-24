ceadc30f1792d7de1e62dab7007f514e286ce830
Dafydd-Rhys Maund <maund.dafyddrhys@gmail.com>
2021-11-24 04:05:49 +0000

    got rid of unused code

--- src/player/Inventory/InventoryInteraction.java
+++ src/player/Inventory/InventoryInteraction.java
@@ -4,7 +4,6 @@ import entity.Item;
 import javafx.application.Platform;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.DragEvent;
@@ -19,16 +18,6 @@ public class InventoryInteraction {
     private static final Item[] items = new Item[32];
     private static int index = 0;
 
-    private static final String dir = System.getProperty("user.dir") + "/src/resources/images/game/entities/";
-    private static final Image bombImage = new Image(dir + "bomb.png");
-    private final Image deathRatImage = new Image(dir + "death-rat.png");
-    private final Image femaleChangeImage = new Image(dir + "female-change.png");
-    private final Image maleChangeImage = new Image(dir + "male-change.png");
-    private final Image gasGrenadeImage = new Image(dir + "gas-grenade.png");
-    private final Image noEntryImage = new Image(dir + "no-entry-sign.png");
-    private final Image poisonImage = new Image(dir + "poison.png");
-    private final Image sterilisationImage = new Image(dir + "sterilisation.png");
-
     public static void draggableImage(Canvas canvas, GraphicsContext gc, AnchorPane abilities, Item item, int amount) {
         ImageView imageView = new ImageView();
         imageView.setImage(item.getImage());
@@ -37,10 +26,12 @@ public class InventoryInteraction {
 
         AnchorPane.setTopAnchor(imageView, item.getYOffset() * 40.0 + 10);
         AnchorPane.setLeftAnchor(imageView, amount * 35.0 + 5);
+
         Platform.runLater(() -> abilities.getChildren().add(imageView));
         images[index] = imageView;
         items[index] = item;
         index++;
+
         System.out.println("added " + item.getEntityName() + " to inventory");
 
         imageView.setOnDragDetected(event -> {
@@ -74,11 +65,13 @@ public class InventoryInteraction {
 
         item.setCurrentPosX(x);
         item.setCurrentPosY(y);
+        System.out.println("Dropped " + item.getEntityName() + " at " + x + ", " + y);
 
         Level.getItems().add(item);
         Level.getTiles()[y][x].addEntityToTile(item);
         Inventory.removeItem(item);
 
+        index -= 1;
         System.arraycopy(items, pos + 1, items, pos, items.length - pos - 1);
         System.arraycopy(images, pos + 1, images, pos, items.length - pos - 1);
         gc.drawImage(image.getImage(), x * 50 + 10, y * 50 + 10);
