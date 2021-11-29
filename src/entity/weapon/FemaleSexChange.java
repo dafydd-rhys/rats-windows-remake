8589c0e4c65c2a069cd57a2349e29c5bd19de028
Dawid W <dawidoskamilos@gmail.com>
2021-11-29 12:08:57 +0000

    Revert "Merge branch 'main' of https://github.com/Stryzhh/rats-windows-remake"
    This reverts commit a374cfa2d53c66df5dfa7a4e630dff42799734a2, reversing
    changes made to ce342ffdd2b7862a60fd9d4dc397815b09d1e346.

--- src/entity/weapon/FemaleSexChange.java
+++ src/entity/weapon/FemaleSexChange.java
@@ -47,18 +47,15 @@ public class FemaleSexChange extends Item {
 
                     if (target.getGender() == Rat.Gender.MALE) {
                         target.setGender(Rat.Gender.FEMALE);
-                        if (target.isSterilised()) {
-                            target.setImage(new Image(System.getProperty("user.dir") +
-                                    "\\src\\resources\\images\\game\\entities\\sterilised-female-rat.png"));
-                        } else {
-                            target.setImage(new Image(System.getProperty("user.dir") +
-                                    "\\src\\resources\\images\\game\\entities\\female-rat.png"));
-                        }
+                        target.setImage(new Image(System.getProperty("user.dir") +
+                                "\\src\\resources\\images\\game\\entities\\female-rat.png"));
                         target.getImages();
+
+                        this.hp -= 1;
+                        level.getItems().remove(this);
+                        entitiesOnTile.remove(this);
+                        break;
                     }
-                    this.hp -= 1;
-                    level.getItems().remove(this);
-                    entitiesOnTile.remove(this);
                 }
             }
         }
--- src/entity/weapon/MaleSexChange.java
+++ src/entity/weapon/MaleSexChange.java
@@ -48,18 +48,15 @@ public class MaleSexChange extends Item {
 
                     if (target.getGender() == Rat.Gender.FEMALE) {
                         target.setGender(Rat.Gender.MALE);
-                        if (target.isSterilised()) {
-                            target.setImage(new Image(System.getProperty("user.dir") +
-                                    "\\src\\resources\\images\\game\\entities\\sterilised-male-rat.png"));
-                        } else {
-                            target.setImage(new Image(System.getProperty("user.dir") +
-                                    "\\src\\resources\\images\\game\\entities\\male-rat.png"));
-                        }
+                        target.setImage(new Image(System.getProperty("user.dir") +
+                                "\\src\\resources\\images\\game\\entities\\male-rat.png"));
                         target.getImages();
+
+                        this.hp -= 1;
+                        level.getItems().remove(this);
+                        entitiesOnTile.remove(this);
+                        break;
                     }
-                    this.hp -= 1;
-                    level.getItems().remove(this);
-                    entitiesOnTile.remove(this);
                 }
             }
         }
