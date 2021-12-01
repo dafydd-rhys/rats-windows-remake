c3febe2c3515148c335ebd7d06ba923333bc6bdd
Ethosss <sdaitoiu@gmail.com>
2021-12-01 00:02:58 +0000

    Added Level win and level fail fx
    Added sound effects for winning or losing a level

--- src/controllers/GameOverController.java
+++ src/controllers/GameOverController.java
@@ -12,6 +12,8 @@ import main.stage.StageFunctions;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.IOException;
+import static main.external.Audio.playGameEffect;
+
 
 public class GameOverController implements Initializable {
 
@@ -42,15 +44,35 @@ public class GameOverController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         if (Level.getGameOver() && Level.getGameWon()) {
             status.setText("You Won!");
+
+            try {
+                playSound("/src/resources/audio/game/level-win.wav");
+            } catch (UnsupportedAudioFileException | LineUnavailableException | IOException e) {
+                e.printStackTrace();
+            }
             if (Level.getCurrentLevel() + 1 <= 6) {
                 play.setDisable(false);
             }
+
         } else if (Level.getGameOver()) {
             status.setText("You Lost!");
+            try {
+                playSound("/src/resources/audio/game/level-fail.wav");
+            } catch (UnsupportedAudioFileException | LineUnavailableException | IOException e) {
+                e.printStackTrace();
+            }
         }
         score.setText("Score: " + Level.getScore());
 
         onActions();
+    }
+
+    public void playSound(String path) throws UnsupportedAudioFileException, LineUnavailableException, IOException {
+        try {
+            playGameEffect(System.getProperty("user.dir") + path);
+        } catch (UnsupportedAudioFileException | LineUnavailableException | IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void onActions() {
