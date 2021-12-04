792010f2c5ce3c6699fcd9eb212bf42dc8480773
DishonestOne <93766384+DishonestOne@users.noreply.github.com>
2021-12-05 03:57:44 +0800

    Score javadoc
    Just only one error on this. get back to it later.

--- src/scoreboard/Score.java
+++ src/scoreboard/Score.java
@@ -4,15 +4,24 @@ import java.io.IOException;
 
 /**
  * The type Score.
+ *
+ * @author Bryan Kok
+ * @author whoever made this idk
  */
 public class Score {
 
-    /** */
-    private final int level;
-    /** */
-    private final String name;
-    /** */
-    private final int score;
+    /**
+     * The score's level.
+     */
+    private int completedLevel;
+    /**
+     * The scorer's name.
+     */
+    private String newName;
+    /**
+     * The score number.
+     */
+    private int newScore;
 
     /**
      * Instantiates a new Score.
@@ -21,10 +30,10 @@ public class Score {
      * @param name  the name
      * @param score the score
      */
-    public Score(int level, String name, int score) {
-        this.level = level;
-        this.name = name;
-        this.score = score;
+    public Score(final int level, final String name, final int score) {
+        this.completedLevel = level;
+        this.newName = name;
+        this.newScore = score;
     }
 
     /**
@@ -33,9 +42,35 @@ public class Score {
      * @throws IOException the io exception
      */
     public void addToScoreBoard() throws IOException {
-        Scoreboard scoreboard = new Scoreboard(level);
+        Scoreboard scoreboard = new Scoreboard(completedLevel);
         scoreboard.getScoreboard();
-        scoreboard.add(new ScoreboardPlayer("Level: " + level, 0, name, score));
+        scoreboard.add(new ScoreboardPlayer("Level: " + completedLevel, 0, newName, newScore));
     }
 
+    /**
+     * Sets the level of the score.
+     *
+     * @param level the level of the score.
+     */
+    public void setCompletedLevel(final int level) {
+        this.completedLevel = level;
+    }
+
+    /**
+     * Sets the scorer's name.
+     *
+     * @param name  the new scorer's name.
+     */
+    public void setNewName(final String name) {
+        this.newName = name;
+    }
+
+    /**
+     * Sets the score number.
+     *
+     * @param score the new score number.
+     * */
+    public void setNewScore(final int score) {
+        this.newScore = score;
+    }
 }
